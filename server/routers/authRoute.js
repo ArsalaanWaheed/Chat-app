5c153a8a42fe7431b08bdb0f989c7e7f32cbbfd9
Arsalaan <arsalaanwaheed2004@gmail.com>
2024-10-04 20:48:53 +0530

    minor

--- server/routers/authRoute.js
+++ server/routers/authRoute.js
@@ -8,7 +8,7 @@ const authRouter=Router()
 authRouter.post("/signup",
  signup)
 
-authRouter.post("/login", (req,res,next)=>{ passport.authenticate("local",(err,user,info)=>{
+authRouter.post("/login", (req,res,next)=>{ passport.authenticate("local",{session:true},(err,user,info)=>{
   if(err) return next(err);
   if(!user){
     return  res.status(401).json({message:"Invalid username or password"});
@@ -17,9 +17,10 @@ authRouter.post("/login", (req,res,next)=>{ passport.authenticate("local",(err,u
   req.login(user,(err)=>{
     if(err) {
       return next(err);}
-      res.send({message:"Logged In Successfully"});
+      res.status(200).json({message:"Logged In Successfully"});
     }
-  )
+  );
+  login
 })})
 
 
